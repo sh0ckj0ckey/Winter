c6c20c56bbedcc8c0b748778a7498fdfb2e2c209
姚一鸣 <yaoyiming123@live.com>
2025-01-17 17:27:16 +0800

    m

--- Winter/Views/MusicPlaylistsPage.xaml.cs
+++ Winter/Views/MusicPlaylistsPage.xaml.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
 using Winter.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -18,6 +21,24 @@ namespace Winter.Views
         {
             _viewModel = App.Current.Services.GetRequiredService<MusicPlaylistsViewModel>();
             this.InitializeComponent();
+        }
+
+        private void Button_PointerEntered(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                Storyboard? sb = btn.Resources["PointerEnterStoryboard"] as Storyboard;
+                sb?.Begin();
+            }
+        }
+
+        private void Button_PointerExited(object sender, Microsoft.UI.Xaml.Input.PointerRoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                Storyboard? sb = btn.Resources["PointerLeaveStoryboard"] as Storyboard;
+                sb?.Begin();
+            }
         }
     }
 }
