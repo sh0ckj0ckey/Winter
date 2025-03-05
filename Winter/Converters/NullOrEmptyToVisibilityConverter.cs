6808c84d551bdaf2aeb6637d064907a31fb0b29f
姚一鸣 <yaoyiming123@live.com>
2025-03-05 23:48:47 +0800

    m

---
+++ Winter/Converters/NullOrEmptyToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+
+namespace Winter.Converters
+{
+    internal class NullOrEmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return parameter?.ToString() != "!" ? (string.IsNullOrWhiteSpace(value?.ToString()) ? Visibility.Collapsed : Visibility.Visible)
+                                                : (string.IsNullOrWhiteSpace(value?.ToString()) ? Visibility.Visible : Visibility.Collapsed);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
